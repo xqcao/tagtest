b2b4ed1c9ee63a17bd7068b39a880c13b47a9939
xqcao <xqcaoumail@gmail.com>
2021-05-13 12:04:54 -0700

    change in v1.3

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask,jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+
+@app.route('/data')
+def helloData():
+    return jsonify({"name":"adam","email":"adam@gmail.com"})
 
 if __name__ == '__main__':
     app.run(port=5001,host="0.0.0.0",debug=False)
